f9da0046332e8a9f3148a8050837f4e6f881c7a7
johannaanden <johannaanden@hotmail.com>
2018-12-11 08:21:31 +0100

    Removes BMI calculator from calculator.index and the logic from calculator.ts, Adds buttons to Metric and Imperial pages insted

--- src/pages/calculator/calculator.ts
+++ src/pages/calculator/calculator.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicPage } from 'ionic-angular';
+import { IonicPage, NavController } from 'ionic-angular';
+import { MetricPage } from '../metric/metric';
+import { ImperialPage } from '../imperial/imperial';
 
 @IonicPage()
 @Component({
@@ -7,37 +9,14 @@ import { IonicPage } from 'ionic-angular';
   templateUrl: 'calculator.html',
 })
 export class CalculatorPage {
-  height: number;
-  weight: number;
-  bmiValue: number;
-  bmiMessage: string;
+  
+  constructor(public navController: NavController){ }
 
-  constructor() {
-  }
-
-  calculateBMI() {
-    if (this.weight > 0 && this.height > 0) {
-      let finalBmi = this.weight / (this.height / 100 * this.height / 100);
-      this.bmiValue = parseFloat(finalBmi.toFixed(2));
-      this.setBMIMessage();
-    }
-  }
-
-  private setBMIMessage() {
-    if (this.bmiValue < 18.5) {
-      this.bmiMessage = "Underweight"
+    goToMetric(){
+      this.navController.setRoot(MetricPage);
     }
 
-    if (this.bmiValue > 18.5 && this.bmiValue < 25) {
-      this.bmiMessage = "Normal"
+    goToImperial(){
+      this.navController.setRoot(ImperialPage);
     }
-
-    if (this.bmiValue > 25 && this.bmiValue < 30) {
-      this.bmiMessage = "Overweight"
-    }
-
-    if (this.bmiValue > 30) {
-      this.bmiMessage = "Obese"
-    }
-  }
 }
