f5e31a967bc512641db7863392387410b0fe32c7
johannaanden <johannaanden@hotmail.com>
2018-12-11 06:06:28 +0100

    Creates imperial page

---
+++ src/pages/imperial/imperial.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { ImperialPage } from './imperial';
+
+@NgModule({
+  declarations: [
+    ImperialPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(ImperialPage),
+  ],
+})
+export class ImperialPageModule {}
