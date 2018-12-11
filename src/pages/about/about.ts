dfc8ff1d9833678393fbb01f1bc76040f851bb4b
johannaanden <johannaanden@hotmail.com>
2018-12-11 10:07:43 +0100

    Adds slide functionality to About page

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,14 +1,24 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ViewChild } from '@angular/core';
+import { Slides } from 'ionic-angular';
 
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html'
 })
 export class AboutPage {
+  @ViewChild(Slides) slides: Slides;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController) { }
 
+  goToSlide() {
+    this.slides.slideTo(2, 500);
+  }
+
+  slideChanged() {
+    let currentIndex = this.slides.getActiveIndex();
+    console.log('Current index is', currentIndex);
   }
 
 }
